07361873ecee55f63a0eb31b596383d5650e4384
forafish <fora.luo@gmail.com>
2018-02-27 19:12:42 +0800

    add helpers

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ if (!global._babelPolyfill) { require('babel-polyfill'); }
 module.exports = require('./lib/index');
 module.exports.entities = require('./lib/entities');
 module.exports.models = require('./lib/models');
+module.exports.helpers = require('./lib/helpers');
