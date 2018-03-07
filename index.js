e7dfc1518ed3f872244ead6236c3e52af4e0a7ec
forafish <fora.luo@gmail.com>
2018-03-08 01:31:48 +0800

    refactoring to getRecursiveRequires/getRecursiveRewards

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ if (!global._babelPolyfill) { require('babel-polyfill'); }
 module.exports = require('./lib/index');
 module.exports.entities = require('./lib/entities');
 module.exports.helpers = require('./lib/helpers');
-module.exports.hooks = require('./lib/hooks');
 module.exports.models = require('./lib/models');
--- src/helpers.js
+++ src/helpers.js
@@ -1,22 +1,22 @@
 import fp from 'mostly-func';
 
-export const getActivityRequires = (activities) => {
+export const getRecursiveRequires = (path) => (activities) => {
   return fp.reduce((arr, activity) => {
     if (activity.type === 'single') {
-      arr.push(activity.requires || []);
+      arr.push(fp.dotPath(path, activity) || []);
     } else {
-      arr.push(fp.flatten(getActivityRequires(activity.activities || [])));
+      arr.push(fp.flatten(getRecursiveRequires(path)(activity.activities || [])));
     }
     return arr;
   }, [], activities);
 };
 
-export const getActivityRewards = (activities) => {
+export const getRecursiveRewards = (path) => (activities) => {
   return fp.reduce((arr, activity) => {
     if (activity.type === 'single') {
-      return arr.concat(activity.rewards || []);
+      return arr.concat(fp.dotPath(path, activity) || []);
     } else {
-      return arr.concat(fp.flatten(getActivityRewards(activity.activities || [])));
+      return arr.concat(fp.flatten(getRecursiveRewards(path)(activity.activities || [])));
     }
   }, [], activities);
 };
