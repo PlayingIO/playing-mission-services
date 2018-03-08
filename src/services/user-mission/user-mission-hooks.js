ea09debc212388bd41ad5dad86fe22e5cfaedb3c
forafish <fora.luo@gmail.com>
2018-03-08 11:31:01 +0800

    add hooks for user-mission

--- src/services/user-mission/user-mission-hooks.js
+++ src/services/user-mission/user-mission-hooks.js
@@ -1,4 +1,6 @@
+import { iff, isProvider } from 'feathers-hooks-common';
 import { hooks as auth } from 'feathers-authentication';
+import { associateCurrentUser, queryWithCurrentUser } from 'feathers-authentication-hooks';
 import { hooks } from 'mostly-feathers-mongoose';
 import UserMissionEntity from '~/entities/user-mission-entity';
 
@@ -6,17 +8,24 @@ module.exports = function(options = {}) {
   return {
     before: {
       all: [
+        hooks.addParams({ $auth: { query: { $select: 'scores,actions,*' } } }),
         auth.authenticate('jwt')
       ],
+      create: [
+        iff(isProvider('external'),
+          associateCurrentUser({ idField: 'id', as: 'owner' }))
+      ],
       update: [
-        hooks.discardFields('id', 'createdAt', 'updatedAt', 'destroyedAt')
+        hooks.discardFields('id', 'owner', 'createdAt', 'updatedAt', 'destroyedAt')
       ],
       patch: [
-        hooks.discardFields('id', 'createdAt', 'updatedAt', 'destroyedAt')
+        hooks.discardFields('id', 'owner', 'createdAt', 'updatedAt', 'destroyedAt')
       ]
     },
     after: {
       all: [
+        hooks.populate('owner', { service: 'users' }),
+        hooks.populate('performers.user', { service: 'users' }),
         hooks.presentEntity(UserMissionEntity, options),
         hooks.responder()
       ]
