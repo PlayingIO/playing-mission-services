13a8f9c92ba0233c07ebec6aca73af463745480b
forafish <fora.luo@gmail.com>
2018-03-17 23:50:05 +0800

    add cache to mission service

--- src/services/user-mission/user-mission-hooks.js
+++ src/services/user-mission/user-mission-hooks.js
@@ -1,13 +1,16 @@
 import { iff, isProvider } from 'feathers-hooks-common';
 import { associateCurrentUser, queryWithCurrentUser } from 'feathers-authentication-hooks';
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
+
 import UserMissionEntity from '~/entities/user-mission-entity';
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
-        hooks.authenticate('jwt', options.auth, 'scores,actions')
+        hooks.authenticate('jwt', options.auth, 'scores,actions'),
+        cache(options.cache)
       ],
       create: [
         iff(isProvider('external'),
@@ -29,6 +32,7 @@ module.exports = function(options = {}) {
         hooks.populate('mission', { service: 'missions' }),
         hooks.populate('owner', { service: 'users' }),
         hooks.populate('performers.user', { service: 'users' }),
+        cache(options.cache),
         hooks.presentEntity(UserMissionEntity, options),
         hooks.responder()
       ]
