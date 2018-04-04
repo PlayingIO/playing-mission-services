13202cc22fa7f7599ac5e01265ac8377ef9404e7
forafish <fora.luo@gmail.com>
2018-04-05 02:49:02 +0800

    add user-mission's events

---
+++ src/services/user-mission/user-mission.events.js
@@ -0,0 +1,31 @@
+const createActivity = async function (app, userMission, verb, message) {
+  const svcFeeds = app.service('feeds');
+
+  const activity = {
+    actor: `user:${userMission.user}`,
+    verb: verb,
+    object: `mission:${userMission.mission}`,
+    foreignId: `userMission:${userMission.id}`,
+    message: message
+  };
+
+  await Promise.all([
+    // add to game's actvity log
+    svcFeeds.action('addActivity').patch(`game:playread`, activity),
+    // add to player's activity log
+    svcFeeds.action('addActivity').patch(`user:${userMission.user}`, activity),
+  ]);
+};
+
+// subscribe to mission.create events
+export default function (app, options) {
+  app.trans.add({
+    topic: 'playing.events',
+    cmd: 'mission.create'
+  }, (resp) => {
+    const userMission = resp.event;
+    if (userMission) {
+      createActivity(app, userMission, 'mission.create', 'Create a mission');
+    }
+  });
+}
