cfa7c6cf75023c1fd94abbe747ebe7becf9b630e
forafish <fora.luo@gmail.com>
2018-04-11 19:09:09 +0800

    notifier mission.role event

--- src/services/user-mission/user-mission.hooks.js
+++ src/services/user-mission/user-mission.hooks.js
@@ -46,7 +46,8 @@ export default function (options = {}) {
       patch: [
         iff(hooks.isAction('join'), notifier('mission.join')),
         iff(hooks.isAction('leave'), notifier('mission.leave')),
-        iff(hooks.isAction('play'), notifier('mission.play'))
+        iff(hooks.isAction('play'), notifier('mission.play')),
+        iff(hooks.isAction('roles'), notifier('mission.role'))
       ],
       remove: [
         notifier('mission.delete')
