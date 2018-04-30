819db73cf76f277fb62612d859791b0b55884b7c
forafish <fora.luo@gmail.com>
2018-05-01 03:31:05 +0800

    add user-mission-activities service

---
+++ src/services/user-mission-activity/user-mission-activity.service.js
@@ -0,0 +1,43 @@
+import assert from 'assert';
+import makeDebug from 'debug';
+import fp from 'mostly-func';
+import { helpers as metrics } from 'playing-metric-services';
+import { helpers as rules } from 'playing-rule-services';
+import { helpers as feeds } from 'playing-feed-services';
+
+import defaultHooks from './user-mission-activity.hooks';
+
+const debug = makeDebug('playing:mission-services:user-mission-activities');
+
+const defaultOptions = {
+  name: 'user-mission-activities'
+};
+
+export class UserMissionActivityService {
+  constructor (options) {
+    this.options = Object.assign({}, defaultOptions, options);
+    this.name = this.options.name;
+  }
+
+  setup (app) {
+    this.app = app;
+    this.hooks(defaultHooks(this.options));
+  }
+
+  /**
+   * Get a user mission's activity feed
+   */
+  async find (params) {
+    const userMission = params.origin;
+    assert(userMission, 'User mission not exists.');
+
+    const svcFeeds = this.app.service('feeds');
+    return svcFeeds.action('activities').get(`mission:${userMission.id}`, params);
+  }
+}
+
+export default function init (app, options, hooks) {
+  return new UserMissionActivityService(options);
+}
+
+init.Service = UserMissionActivityService;
