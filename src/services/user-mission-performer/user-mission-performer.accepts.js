c3e2f18502c82b65c411958f5f0ae979a0234ab3
forafish <fora.luo@gmail.com>
2018-05-04 19:20:35 +0800

    validates of performers join/leave

---
+++ src/services/user-mission-performer/user-mission-performer.accepts.js
@@ -0,0 +1,27 @@
+import fp from 'mostly-func';
+import { helpers } from 'mostly-feathers-validate';
+import { defaultRoles, rolesExists } from '../../helpers';
+
+export default function accepts (context) {
+  const svcUserMissions = context.app.service('user-missions');
+  const svcUsers = context.app.service('users');
+
+  // validation rules
+  const roles = { arg: 'roles', type: 'object',
+    validates: {
+      exists: rolesExists(svcUserMissions, 'id', 'Roles is invalid') },
+    default: defaultRoles(svcUserMissions, 'id'),
+    required: true, description: 'Role and lanes ' };
+
+  const player = { arg: 'player', type: 'string',
+    validates: {
+      exists: helpers.idExists(svcUsers, 'player', 'Player is not exists') },
+    required: true, description: 'Player' };
+  const user = { arg: 'user', type: 'string', required: true, description: 'Current user' };
+
+  return {
+    create: [ user, roles ],
+    remove: [ user ],
+    kick: [ player ]
+  };
+}
