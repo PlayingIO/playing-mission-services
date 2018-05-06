9ea0d36734e73464af9385634aa1ac4682579689
forafish <fora.luo@gmail.com>
2018-05-07 04:18:18 +0800

    list user mission tasks for current player

--- src/services/user-mission-task/user-mission-task.hooks.js
+++ src/services/user-mission-task/user-mission-task.hooks.js
@@ -5,8 +5,12 @@ export default function (options = {}) {
   return {
     before: {
       all: [
-        hooks.authenticate('jwt', options.auth),
+        hooks.authenticate('jwt', options.auth, 'scores,actions'),
         cache(options.cache)
+      ],
+      find: [
+        hooks.primaryResource('userMission', { service: 'user-missions',
+          select: 'mission.activities.requires,mission.activities.rewards,*' }),
       ]
     },
     after: {
--- src/services/user-mission-task/user-mission-task.service.js
+++ src/services/user-mission-task/user-mission-task.service.js
@@ -1,8 +1,10 @@
 import assert from 'assert';
 import makeDebug from 'debug';
+import { helpers } from 'mostly-feathers-mongoose';
 import fp from 'mostly-func';
 
 import defaultHooks from './user-mission-task.hooks';
+import { walkThroughTasks } from '../../helpers';
 
 const debug = makeDebug('playing:mission-services:user-missions/tasks');
 
@@ -19,6 +21,19 @@ export class UserMissionTaskService {
   setup (app) {
     this.app = app;
     this.hooks(defaultHooks(this.options));
+  }
+
+  /**
+   * Get a list of all available tasks a player can play in a user mission
+   */
+  async find (params) {
+    const userMission = params.userMission;
+    const mission = userMission.mission;
+    if (mission && mission.activities) {
+      return walkThroughTasks(params.user, userMission.tasks)(mission.activities);
+    } else {
+      return [];
+    }
   }
 }
 
