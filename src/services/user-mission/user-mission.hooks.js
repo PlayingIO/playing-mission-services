10a7ad6ccc75815d5bf952fef93971a3809f8494
forafish <fora.luo@gmail.com>
2018-05-07 02:32:25 +0800

    remove notify for invite/roles

--- src/services/user-mission/user-mission.hooks.js
+++ src/services/user-mission/user-mission.hooks.js
@@ -70,9 +70,7 @@ export default function (options = {}) {
         iff(hooks.isAction('create', feeds.notify('mission.create', notifiers)))
       ],
       patch: [
-        iff(hooks.isAction('invite'), feeds.notify('mission.invite', notifiers)),
         iff(hooks.isAction('play'), feeds.notify('mission.play', notifiers)),
-        iff(hooks.isAction('roles'), feeds.notify('mission.roles', notifiers)),
         iff(hooks.isAction('transfer'), feeds.notify('mission.transfer', notifiers))
       ],
       remove: [
