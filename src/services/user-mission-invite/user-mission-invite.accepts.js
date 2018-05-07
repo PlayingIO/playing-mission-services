629c9c4d03c9927012fc78fbe13d8d2cdd671691
forafish <fora.luo@gmail.com>
2018-05-07 19:17:19 +0800

    code format

--- src/services/user-mission-invite/user-mission-invite.accepts.js
+++ src/services/user-mission-invite/user-mission-invite.accepts.js
@@ -8,21 +8,19 @@ export default function accepts (context) {
 
   // validation rules
   const roles = { arg: 'roles', type: 'object',
-    validates: {
-      exists: rolesExists(svcUserMissions, 'primary', 'Roles is invalid') },
+    validates: { exists: rolesExists(svcUserMissions, 'primary', 'Roles is invalid') },
     default: defaultRoles(svcUserMissions, 'primary'),
     required: true, description: 'Role and lanes ' };
 
   const player = { arg: 'player', type: 'string',
-    validates: {
-      exists: helpers.idExists(svcUsers, 'player', 'Player is not exists') },
+    validates: { exists: helpers.idExists(svcUsers, 'player', 'Player is not exists') },
     required: true, description: 'Player' };
   const user = { arg: 'user', type: 'string', required: true, description: 'Current user' };
 
   const invite = { arg: 'invite', type: 'string', required: true, description: 'Invite id' };
 
   return {
-    remove: [ invite ],
     create: [ user, player, roles ],
+    remove: [ invite ]
   };
 }
