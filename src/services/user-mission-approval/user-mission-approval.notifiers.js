03adc18744cddbf99780b1e6c7c3f08ccb838f50
forafish <fora.luo@gmail.com>
2018-05-08 17:51:11 +0800

    notify mission.join.accept for approving roles change

--- src/services/user-mission-approval/user-mission-approval.notifiers.js
+++ src/services/user-mission-approval/user-mission-approval.notifiers.js
@@ -8,24 +8,30 @@ const acceptMission = (context) => {
   const userMission = helpers.getHookData(context);
   const { activity } = context.params.locals;
   const actor = context.params.user.id;
-  const player = helpers.getId(activity.player);
+  const player = helpers.getId(activity.actor);
+  const notifications = performersNotifications(userMission.performers);
+  const custom = {
+    actor: `user:${actor}`,
+    player: `user:${player}`
+  };
   if (activity.verb === 'mission.roles.request') {
-    const notifications = performersNotifications(userMission.performers);
-    const custom = {
-      actor: `user:${actor}`,
-      verb: 'mission.roles.accept',
-      message: 'Change roles request accept',
-      roles: activity.roles,
-      player: `user:${player}`
-    };
-    return [
-      createMissionActivity(context, userMission, custom),
-      `user:${player}`,              // add to player's activity log
-      `notification:${player}`,      // add to player's notification stream
-      `user:${userMission.owner}`,   // add to approver's activity log
-      notifications                  // add to all performers' notification stream
-    ];
+    custom.verb = 'mission.roles.accept';
+    custom.message = 'Change roles request accept';
+    custom.roles = activity.roles;
   }
+  if (activity.verb === 'mission.join.request') {
+    custom.verb = 'mission.join.accept';
+    custom.message = 'Join request accept';
+    custom.roles = activity.roles;
+  }
+  return [
+    createMissionActivity(context, userMission, custom),
+    `user:${player}`,              // add to player's activity log
+    `notification:${player}`,      // add to player's notification stream
+    `user:${userMission.owner}`,   // add to approver's activity log
+    `mission:${userMission.id}`,   // add to mission's activity log
+    notifications                  // add to all performers' notification stream
+  ];
 };
 
 export default {
