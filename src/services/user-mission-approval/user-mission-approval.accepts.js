5eaf036d4558b016f28e87557f9e4dcbf4025efd
forafish <fora.luo@gmail.com>
2018-05-25 11:59:57 +0800

    remove unused imports

--- src/services/user-mission-approval/user-mission-approval.accepts.js
+++ src/services/user-mission-approval/user-mission-approval.accepts.js
@@ -1,7 +1,3 @@
-import fp from 'mostly-func';
-import { helpers } from 'mostly-feathers-validate';
-import { defaultRoles, rolesExists } from '../../helpers';
-
 export default function accepts (context) {
   // validation rules
   const request = { arg: 'id', type: 'string', required: true, description: 'Request id' };
--- src/services/user-mission-task/user-mission-task.accepts.js
+++ src/services/user-mission-task/user-mission-task.accepts.js
@@ -1,7 +1,3 @@
-import fp from 'mostly-func';
-import { helpers } from 'mostly-feathers-validate';
-import { defaultLane, defaultRoles, rolesExists } from '../../helpers';
-
 export default function accepts (context) {
   // validation rules
   const trigger = { arg: 'trigger', type: 'string', required: true, description: 'Id of trigger' };
