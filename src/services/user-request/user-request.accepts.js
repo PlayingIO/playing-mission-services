cc810c097df3e8c0f15a535964750005e54cef89
forafish <fora.luo@gmail.com>
2018-05-25 16:41:31 +0800

    move user-invites/user-requests to playing-interaction-services

--- src/services/user-request/user-request.accepts.js
+++
@@ -1,8 +0,0 @@
-export default function accepts (context) {
-  // validation rules
-  const request = { arg: 'id', type: 'string', required: true, description: 'Request id' };
-
-  return {
-    remove: [ request ]
-  };
-}
